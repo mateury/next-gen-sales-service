abb54412ada5128a710cf24d8867243ac3e84a03
rybakm4 <mateusz.rybak@play.pl>
2025-10-24 11:07:46 +0200

    first commit

---
+++ src/main/java/pl/warsaw/hackaton/nextgensalesservice/NextGenSalesServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.warsaw.hackaton.nextgensalesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextGenSalesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextGenSalesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/warsaw/hackaton/nextgensalesservice/NextGenSalesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.warsaw.hackaton.nextgensalesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextGenSalesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
